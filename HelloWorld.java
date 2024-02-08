6f74edf95c6884b516f21dd7045268d25a862699
Jessah Tumbaga <jessah.tumbaga@stu.ocsb.ca>
2024-02-08 11:26:57 +0000

    Added Hello.World.java

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+* Greats self and additional comment
+
+* @authorJessah
+
+* @version 1.0
+
+* @ 2024 Feb 7
+
+*/
+
+
+
+
+
+class HelloWorld {
+
+    public static void main(String[] args) { // print conversation
+
+        System.out.println("Hello myself,"); 
+        System.out.println("I hope you are doing well");       
+
+    }
+
+}
